a73ce4ac6f7e30c5c0cd5bf1638ce222e1594eb8
kms06 <kms0644804@naver.com>
2021-02-01 18:35:05 +0900

    초기 커밋.

---
+++ UnipointAutomation/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+using mshtml;
+using SHDocVw;
+
+namespace UnipointAutomation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var ie = new InternetExplorer();
+            var webBrowser = (IWebBrowserApp)ie;
+            webBrowser.Visible = true;
+            webBrowser.Navigate("http://gw.unipoint.co.kr");
+            Thread.Sleep(3000);
+            IHTMLDocument3 doc = (IHTMLDocument3)webBrowser.Document;
+            IHTMLElement id = doc.getElementById("UserName");
+            IHTMLElement pw = doc.getElementById("Password");
+            id.setAttribute("value", "아이디");
+            pw.setAttribute("value", "비밀번호");
+            IHTMLElementCollection tagList = doc.getElementsByTagName("a");
+            foreach (IHTMLElement tag in tagList)
+            {
+                tag.click();
+                break;
+            }
+        }
+
+    }
+}
