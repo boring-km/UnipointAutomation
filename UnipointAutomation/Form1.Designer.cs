7812cd25b9bd8388c492a613d60889f5b62d8be8
kms06 <kms0644804@naver.com>
2021-02-02 16:22:16 +0900

    [UA-01-km] 닫기 버튼 누락된 기능 구현

--- UnipointAutomation/Form1.Designer.cs
+++ UnipointAutomation/Form1.Designer.cs
@@ -107,6 +107,7 @@ namespace UnipointAutomation
             this.button3.TabIndex = 6;
             this.button3.Text = "X";
             this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // label3
             // 
